f77a01bb62449d2e644f0c553b80b2b0432badee
zenywallet <zenywallet@bitzeny.jp>
2023-09-04 01:58:00 +0900

    change zenyjs externs

--- src/zenyjs/zenyjs_externs.nim
+++ src/zenyjs/zenyjs_externs.nim
@@ -1,7 +1,7 @@
 # Copyright (c) 2022 zenywallet
 
-echo """
-var externs = {
+const ZenyJsExterns* = """
+var zenyjsexterns = {
   zenyjsMod: {
     onRuntimeInitialized: function() {},
     preRun: [],
@@ -80,44 +80,3 @@ var jq = {
   val: function() {}
 };
 """
-
-import os
-import nre
-import tables
-import strutils
-
-var list = initOrderedTable[string, string]()
-var resList = initOrderedTable[string, string]()
-
-let targetJs = paramStr(1)
-var d = readFile(targetJs)
-
-for s in d.findIter(re""": "[a-zA-Z_][\w]*""""):
-  var t = s.match.strip(chars = {' ', ':', '"'})
-  list[t] = t
-
-for s in d.findIter(re"[a-zA-Z_][\w]*: "):
-  var t = s.match.strip(chars = {' ', ':'})
-  list[t] = t
-
-for s in d.findIter(re"\.[a-zA-Z_][\w]*"):
-  #echo s.match
-  var t = s.match.strip(chars = {'.'})
-  if list.hasKey(t):
-    resList[t] = t
-
-resList.sort(system.cmp)
-
-var res: string
-res.add("var externs2 = {\n")
-var last = resList.len
-var i = 0
-for s in resList.keys:
-  inc(i)
-  if i == last:
-    res.add("  " & s & ": 0\n")
-  else:
-    res.add("  " & s & ": 0,\n")
-res.add("};\n")
-
-echo res
