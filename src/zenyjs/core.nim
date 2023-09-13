0fa72fde4ea80ba7b30913e82f01eab65d9d6e33
zenywallet <zenywallet@bitzeny.jp>
2023-09-14 01:09:25 +0900

    change core js

--- src/zenyjs/core.nim
+++ src/zenyjs/core.nim
@@ -1,9 +1,10 @@
 # Copyright (c) 2023 zenywallet
 
-import std/asyncjs
-import std/jsffi except `.=`
-import zenyjs/jslib except Array
+when defined(js):
+  import std/asyncjs
+  import std/jsffi except `.=`
+  import zenyjs/jslib except Array
 
-export asyncjs
-export jsffi
-export jslib
+  export asyncjs
+  export jsffi
+  export jslib
