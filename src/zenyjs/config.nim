68d4345bf2e69b364ac3df68ebdf5a35284ca7a4
zenywallet <zenywallet@bitzeny.jp>
2023-09-14 01:27:04 +0900

    change config networks const to var

--- src/zenyjs/config.nim
+++ src/zenyjs/config.nim
@@ -1,6 +1,7 @@
 # Copyright (c) 2023 zenywallet
 
 import macros
+import arraylib
 
 type
   Network* = object
@@ -11,20 +12,26 @@ type
     bech32Extra*: seq[string]
     testnet*: bool
 
+  NetworkId* = int
+
+var Networks*: Array[Network]
+
+var curNetworkId {.compileTime.} = 0
+
 macro networks*(networkConfig: untyped): untyped =
-  var networkIdEnum = nnkEnumTy.newTree(newEmptyNode())
-  var networkBracket = nnkBracket.newTree()
+  var networkStmt = newStmtList()
   for n in networkConfig:
-    networkIdEnum.add(n[0])
+    var networkId = n[0]
+    networkStmt.add quote do:
+      const `networkId`* = `curNetworkId`.NetworkId
+    inc(curNetworkId)
     var networkObj = nnkObjConstr.newTree(newIdentNode("Network"))
     for c in n[1]:
       networkObj.add(nnkExprColonExpr.newTree(c[0], c[1][0]))
-    networkBracket.add(networkObj)
-  var NetworkId = ident("NetworkId")
-  var Networks = ident("Networks")
-  result = quote do:
-    type `NetworkId`* {.pure.} = `networkIdEnum`
-    const `Networks`* = `networkBracket`
+    var Networks = ident("Networks")
+    networkStmt.add quote do:
+      `Networks`.add(`networkObj`)
+  result = networkStmt
 
 networks:
   BitZeny_mainnet:
