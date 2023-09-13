7adf98ad92ce86a9bd215882853c3bfffaf776b7
zenywallet <zenywallet@bitzeny.jp>
2023-09-14 00:23:25 +0900

    change config networks macro

--- src/zenyjs/config.nim
+++ src/zenyjs/config.nim
@@ -1,5 +1,7 @@
 # Copyright (c) 2023 zenywallet
 
+import macros
+
 type
   Network* = object
     pubKeyPrefix*: uint8
@@ -9,21 +11,33 @@ type
     bech32Extra*: seq[string]
     testnet*: bool
 
-  NetworkId* {.pure.} = enum
-    BitZeny_mainnet
-    BitZeny_testnet
+macro networks*(networkConfig: untyped): untyped =
+  var networkIdEnum = nnkEnumTy.newTree(newEmptyNode())
+  var networkBracket = nnkBracket.newTree()
+  for n in networkConfig:
+    networkIdEnum.add(n[0])
+    var networkObj = nnkObjConstr.newTree(newIdentNode("Network"))
+    for c in n[1]:
+      networkObj.add(nnkExprColonExpr.newTree(c[0], c[1][0]))
+    networkBracket.add(networkObj)
+  var NetworkId = ident("NetworkId")
+  var Networks = ident("Networks")
+  result = quote do:
+    type `NetworkId`* {.pure.} = `networkIdEnum`
+    const `Networks`* = `networkBracket`
 
-const Networks* = [
-  Network(
-    pubKeyPrefix: 81'u8,
-    scriptPrefix: 5'u8,
-    wif: 128'u8,
-    bech32: "sz",
-    bech32Extra: @["bz"],
-    testnet: false),
-  Network(
-    pubKeyPrefix: 111'u8,
-    scriptPrefix: 196'u8,
-    wif: 239'u8,
-    bech32: "tz",
-    testnet: true)]
+networks:
+  BitZeny_mainnet:
+    pubKeyPrefix: 81'u8
+    scriptPrefix: 5'u8
+    wif: 128'u8
+    bech32: "sz"
+    bech32Extra: @["bz"]
+    testnet: false
+
+  BitZeny_testnet:
+    pubKeyPrefix: 111'u8
+    scriptPrefix: 196'u8
+    wif: 239'u8
+    bech32: "tz"
+    testnet: true
