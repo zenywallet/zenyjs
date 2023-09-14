4bfffb5e454d03aa1fd6b96ba4674a7991b4658c
zenywallet <zenywallet@bitzeny.jp>
2023-09-15 08:44:23 +0900

    change zenyjs import

--- src/zenyjs.nim
+++ src/zenyjs.nim
@@ -4,21 +4,27 @@ when defined(js):
   import zenyjs/zenyjs
   export zenyjs
 
+elif defined(emscripten):
+  import zenyjs/zenyjs
+  export zenyjs
+
 else:
   import std/os
+  import zenyjs/zenyjs
   import zenyjs/contents
   import zenyjs/zenyjs_externs
+  export zenyjs
 
   const ZenyWasm = staticRead(currentSourcePath().parentDir() / "zenyjs/zenyjs.wasm")
 
   proc staticZenyJs*(src: static string): tuple[js: string, wasm: string] {.compileTime.} =
-    let minJs = zenyjs.contents.scriptMinifier(src, extern = ZenyJsExterns)
+    let minJs = contents.scriptMinifier(src, extern = ZenyJsExterns)
     (js: minJs, wasm: ZenyWasm)
 
   proc staticZenyJs*(src, extern: static string): tuple[js: string, wasm: string] {.compileTime.} =
-    let minJs = zenyjs.contents.scriptMinifier(src, extern = extern & "\n" & ZenyJsExterns)
+    let minJs = contents.scriptMinifier(src, extern = extern & "\n" & ZenyJsExterns)
     (js: minJs, wasm: ZenyWasm)
 
   proc staticZenyJs*(src: static string, extern: static seq[string]): tuple[js: string, wasm: string] {.compileTime.} =
-    let minJs = zenyjs.contents.scriptMinifier(src, extern = generateExternCode(extern) & "\n" & ZenyJsExterns)
+    let minJs = contents.scriptMinifier(src, extern = generateExternCode(extern) & "\n" & ZenyJsExterns)
     (js: minJs, wasm: ZenyWasm)
