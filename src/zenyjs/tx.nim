206a47c65983c9a79e100c55fe31083761987450
zenywallet <zenywallet@bitzeny.jp>
2024-07-25 00:16:45 +0900

    move to tx_types

--- src/zenyjs/tx.nim
+++ src/zenyjs/tx.nim
@@ -69,30 +69,8 @@ else:
   import custom
   import macros
   import config
+  import tx_types
   export custom
-
-  type
-    Flags* = distinct uint8
-
-    Witness* = distinct Array[byte]
-
-    Sig* = distinct Array[byte]
-
-    TxIn* = tuple[tx: Hash, n: uint32, sig: Sig, sequence: uint32]
-
-    TxOut* = tuple[value: uint64, script: Script]
-
-    TxObj* = object
-      ver*: int32
-      flags*: Flags
-      ins*: Array[TxIn]
-      outs*: Array[TxOut]
-      witnesses*: Array[Array[Witness]]
-      locktime*: uint32
-
-    TxHandle* = ptr TxObj
-
-    Tx* = object of HandleObj[TxHandle]
 
   const
     SIGHASH_ALL* = 1
@@ -103,34 +81,6 @@ else:
     SIGHASH_DEFAULT* = 0
     SIGHASH_OUTPUT_MASK* = 3
     SIGHASH_INPUT_MASK* = 0x80
-
-  proc free*(tx: Tx) {.exportc: "tx_$1".} =
-    if tx.handle.isNil: return
-    let tx = tx.handle
-    `=destroy`(tx.witnesses)
-    `=destroy`(tx.outs)
-    `=destroy`(tx.ins)
-    tx.deallocShared()
-
-  proc `=destroy`*(tx: var Tx) = tx.free()
-
-  proc `=copy`*(a: var Tx; b: Tx) =
-    if a.handle == b.handle: return
-    `=destroy`(a)
-    wasMoved(a)
-    if b.handle != nil:
-      a.handle = cast[typeof(a.handle)](allocShared0(sizeof(TxObj)))
-      a.handle.ver = b.handle.ver
-      a.handle.flags = b.handle.flags
-      a.handle.ins = b.handle.ins
-      a.handle.outs = b.handle.outs
-      a.handle.witnesses = b.handle.witnesses
-      a.handle.locktime = b.handle.locktime
-
-  proc `=sink`*(a: var Tx; b: Tx) =
-    `=destroy`(a)
-    wasMoved(a)
-    a.handle = b.handle
 
   proc duplicate*(node: Tx): Tx =
     var h = cast[TxHandle](allocShared0(sizeof(TxObj)))
--- src/zenyjs/tx_types.nim
+++ src/zenyjs/tx_types.nim
@@ -1,31 +1,33 @@
 # Copyright (c) 2020 zenywallet
 
+import arraylib
+import script
+import hash
+export hash
+
+type
+  Flags* = distinct uint8
+
+  Witness* = distinct Array[byte]
+
+  Sig* = distinct Array[byte]
+
+  TxIn* = tuple[tx: Hash, n: uint32, sig: Sig, sequence: uint32]
+
+  TxOut* = tuple[value: uint64, script: Script]
+
+  TxObj* = object
+    ver*: int32
+    flags*: Flags
+    ins*: Array[TxIn]
+    outs*: Array[TxOut]
+    witnesses*: Array[Array[Witness]]
+    locktime*: uint32
+
 when defined(js):
   import std/jsffi
-  import arraylib
-  import script
-  import hash
-  export hash
 
   type
-    Flags* = distinct uint8
-
-    Witness* = distinct Array[byte]
-
-    Sig* = distinct Array[byte]
-
-    TxIn* = tuple[tx: Hash, n: uint32, sig: Sig, sequence: uint32]
-
-    TxOut* = tuple[value: uint64, script: Script]
-
-    TxObj* = object
-      ver*: int32
-      flags*: Flags
-      ins*: Array[TxIn]
-      outs*: Array[TxOut]
-      witnesses*: Array[Array[Witness]]
-      locktime*: uint32
-
     Tx* = object
       handle*: JsObject
 
@@ -45,3 +47,39 @@ when defined(js):
     `=destroy`(a)
     if not b.handle.isNil:
       a.handle = b.handle
+
+else:
+  import custom
+
+  type
+    TxHandle* = ptr TxObj
+
+    Tx* = object of HandleObj[TxHandle]
+
+  proc free*(tx: Tx) {.exportc: "tx_$1".} =
+    if tx.handle.isNil: return
+    let tx = tx.handle
+    `=destroy`(tx.witnesses)
+    `=destroy`(tx.outs)
+    `=destroy`(tx.ins)
+    tx.deallocShared()
+
+  proc `=destroy`*(tx: var Tx) = tx.free()
+
+  proc `=copy`*(a: var Tx; b: Tx) =
+    if a.handle == b.handle: return
+    `=destroy`(a)
+    wasMoved(a)
+    if b.handle != nil:
+      a.handle = cast[typeof(a.handle)](allocShared0(sizeof(TxObj)))
+      a.handle.ver = b.handle.ver
+      a.handle.flags = b.handle.flags
+      a.handle.ins = b.handle.ins
+      a.handle.outs = b.handle.outs
+      a.handle.witnesses = b.handle.witnesses
+      a.handle.locktime = b.handle.locktime
+
+  proc `=sink`*(a: var Tx; b: Tx) =
+    `=destroy`(a)
+    wasMoved(a)
+    a.handle = b.handle
