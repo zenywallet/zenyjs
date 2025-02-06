cd937bc9541138ce6e0227c6c571d18db078d99c
zenywallet <zenywallet@bitzeny.jp>
2025-02-07 07:50:42 +0900

    add hash js to string

--- src/zenyjs/hash.nim
+++ src/zenyjs/hash.nim
@@ -4,3 +4,18 @@ import arraylib
 
 type
   Hash* {.borrow: `.`.} = distinct Array[byte]
+
+when defined(js):
+  import jsffi
+  import jslib except Array
+  import algorithm
+
+  borrowArrayProc(Hash)
+
+  proc `$`*(data: Hash): string =
+    var b = cast[Array[byte]](data).toSeq
+    algorithm.reverse(b)
+    var a = newArray[byte](data.len)
+    for i in 0..<b.len:
+      a[i] = b[i]
+    $a.toHex
