8f2f84b56432161c6ac9c427fac82d7b641696a1
zenywallet <zenywallet@bitzeny.jp>
2025-11-12 01:00:56 +0900

    add exec non-static compile and minify

--- src/zenyjs/exec.nim
+++ src/zenyjs/exec.nim
@@ -5,6 +5,11 @@ import std/macros
 import std/strformat
 import std/strutils
 import std/compilesettings
+import std/random
+import std/osproc
+
+type
+  ExecError* = object of CatchableError
 
 const srcFile = currentSourcePath()
 const (srcFileDir, srcFileName, srcFileExt) = splitFile(srcFile)
@@ -18,15 +23,34 @@ buildExecHelper()
 
 proc randomStrCT*(): string {.compileTime.} = staticExec(execHelperExe & " randomstr")
 
+proc randomStr*(): string =
+  const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  var r = initRand()
+  result = newString(13)
+  for i in 0..<result.len:
+    result[i] = r.sample(letters)
+
 proc rmFileCT*(filename: string) {.compileTime.} =
   echo staticExec(execHelperExe & " rmfile " & filename)
+
+proc rmFile*(filename: string) =
+  echo execCmdEx(execHelperExe & " rmfile " & filename).output.strip()
 
 proc basicExternCT*(filename: string): string {.compileTime.} =
   staticExec(execHelperExe & " basicextern " & filename)
 
+proc basicExtern*(filename: string): string  =
+  execCmdEx(execHelperExe & " basicextern " & filename).output.strip()
+
 proc removeTmpFilesCT(removeDir: string) {.compileTime.} =
   var tmpFiles = removeDir / srcFileName & "_tmp" & "[[:digit:]]*"
   var ret = staticExec("find " & tmpFiles & " -type f -mmin +60 -print0 2> /dev/null | xargs -r0 rm")
+  if ret.len > 0:
+    echo ret
+
+proc removeTmpFiles(removeDir: string) =
+  var tmpFiles = removeDir / srcFileName & "_tmp" & "[[:digit:]]*"
+  var ret = execCmdEx("find " & tmpFiles & " -type f -mmin +60 -print0 2> /dev/null | xargs -r0 rm").output.strip()
   if ret.len > 0:
     echo ret
 
@@ -37,6 +61,7 @@ proc removeCacheDirsCT(removeDir: string) {.compileTime.} =
     echo ret
 
 var tmpFileIdCT {.compileTime.}: int = 0
+var tmpFileId: int = 0
 
 proc staticExecCode*(srcFileDir: string, code: string, rstr: string): string {.compileTime.} =
   inc(tmpFileIdCT)
@@ -99,6 +124,19 @@ proc removeThreadVarPatchCT(code: string): string {.compileTime.} =
     else:
       result.add(line & "\n")
 
+proc removeThreadVarPatch(code: string): string =
+  var stage = 0
+  for line in splitLines(code):
+    if stage == 0 and line.startsWith("if (globalThis.") and line.endsWith(" === undefined) {"):
+      stage = 1
+    elif stage == 1:
+      result.add(line.replace("  globalThis.", "var ") & "\n")
+      stage = 2
+    elif stage == 2:
+      stage = 0
+    else:
+      result.add(line & "\n")
+
 proc staticCompileJsCode*(srcFileDir: string, code: string, rstr: string): string {.compileTime.} =
   inc(tmpFileIdCT)
   let tmpNameFile = srcFileDir / srcFileName & "_tmp" & $tmpFileIdCT & rstr
@@ -124,6 +162,32 @@ proc staticCompileJsCode*(srcFileDir: string, code: string, rstr: string): strin
 
 template staticCompileJsCode*(baseDir, code: string): string =
   staticCompileJsCode(baseDir, code, randomStrCT())
+
+proc compileJsCode*(srcFileDir: string, code: string, rstr: string): string =
+  inc(tmpFileId)
+  let tmpNameFile = srcFileDir / srcFileName & "_tmp" & $tmpFileId & rstr
+  let tmpSrcFile = tmpNameFile & srcFileExt
+  let tmpJsFile = tmpNameFile & ".js"
+  writeFile(tmpSrcFile, code)
+  var verbosity = ""
+  let cl = querySetting(SingleValueSetting.commandLine)
+  var params = cl.split(" ")
+  for param in params:
+    if param.startsWith("--verbosity:"):
+      verbosity = " " & param
+      break
+  echo execCmdEx("nim js -d:release --mm:orc" & verbosity & " -o:" & tmpJsFile & " " & tmpSrcFile).output.strip()
+  if not fileExists(tmpJsFile):
+    rmFile(tmpSrcFile)
+    raise newException(ExecError, "nim js failed")
+  result = readFile(tmpJsFile)
+  result = removeThreadVarPatch(result)
+  removeTmpFiles(srcFileDir)
+  rmFile(tmpJsFile)
+  rmFile(tmpSrcFile)
+
+template compileJsCode*(baseDir, code: string): string =
+  compileJsCode(baseDir, code, randomStr())
 
 proc staticMinifyJsCode*(srcFileDir: string, code: string, extern: string, rstr: string): string {.compileTime.} =
   inc(tmpFileIdCT)
@@ -174,6 +238,56 @@ echo $closure_compiler
 
 template staticMinifyJsCode*(baseDir, code, extern: string): string =
   staticMinifyJsCode(baseDir, code, extern, randomStrCT())
+
+proc minifyJsCode*(srcFileDir: string, code: string, extern: string, rstr: string): string =
+  inc(tmpFileId)
+  let tmpNameFile = srcFileDir / srcFileName & "_tmp" & $tmpFileId & rstr
+  let tmpSrcFile = tmpNameFile & ".js"
+  let tmpExtFile = tmpNameFile & "_extern.js"
+  let tmpDstFile = tmpNameFile & "_min.js"
+  writeFile(tmpSrcFile, code)
+  writeFile(tmpExtFile, extern & basicExtern(tmpSrcFile))
+  let downloadClosureCompiler = execCmdEx("""
+ZENYJS_CACHE_DIR=${XDG_CACHE_HOME:-"$HOME/.cache"}/zenyjs
+if [ -x "$(command -v google-closure-compiler)" ]; then
+  echo "download closure-compiler skip"
+elif ls $ZENYJS_CACHE_DIR/closure-compiler-*.jar 1> /dev/null 2>&1; then
+  echo "download closure-compiler skip"
+else
+  mkdir -p $ZENYJS_CACHE_DIR
+  mvn dependency:get -e -Ddest=$ZENYJS_CACHE_DIR -Dartifact=com.google.javascript:closure-compiler:LATEST
+fi
+""").output.strip()
+  echo downloadClosureCompiler
+  let closureCompiler = execCmdEx("""
+ZENYJS_CACHE_DIR=${XDG_CACHE_HOME:-"$HOME/.cache"}/zenyjs
+if [ -x "$(command -v google-closure-compiler)" ]; then
+  closure_compiler="google-closure-compiler"
+elif ls $ZENYJS_CACHE_DIR/closure-compiler-*.jar 1> /dev/null 2>&1; then
+  closure_compiler="java -jar $(ls $ZENYJS_CACHE_DIR/closure-compiler-*.jar | sort -r | head -n1)"
+fi
+echo $closure_compiler
+""").output.strip()
+  if closureCompiler.len > 0:
+    echo "closure compiler: " & closureCompiler
+    let retClosure = execCmdEx(fmt"""
+  {closureCompiler} --compilation_level ADVANCED --jscomp_off=checkVars \
+  --jscomp_off=checkTypes --jscomp_off=uselessCode --js_output_file="{tmpDstFile}" \
+  --externs "{tmpExtFile}" "{tmpSrcFile}" 2>&1 | cut -c 1-240
+  """).output.strip()
+    if retClosure.len > 0:
+      echo retClosure
+    result = readFile(tmpDstFile)
+    rmFile(tmpDstFile)
+  else:
+    echo "closure compiler: not found - skip"
+    result = code
+  removeTmpFiles(srcFileDir)
+  rmFile(tmpExtFile)
+  rmFile(tmpSrcFile)
+
+template minifyJsCode*(baseDir, code, extern: string): string =
+  minifyJsCode(baseDir, code, extern, randomStr())
 
 
 when isMainModule:
